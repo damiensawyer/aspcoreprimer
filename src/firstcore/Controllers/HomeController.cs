036d6a5c00e1a29b84b3ab542b5274d8b83b12e1
Damien Sawyer <damiensawyer@gmail.com>
2017-03-13 01:35:26 +1100

    data from postgres

--- src/firstcore/Controllers/HomeController.cs
+++ src/firstcore/Controllers/HomeController.cs
@@ -1,15 +1,23 @@
 using System;
 using System.Collections.Generic;
+using System.Data;
 using System.Linq;
+using System.Threading.Tasks;
+using Dapper;
 using firstcore;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
+using Npgsql;
+
 public class HomeController : Controller
 {
     private IGreeter greeter;
+    private IConfiguration configuration;
 
-    public HomeController(IGreeter greeter)
+    public HomeController(IGreeter greeter, IConfiguration configuration)
     {
         this.greeter = greeter;
+        this.configuration = configuration;
     }
     public IActionResult Index()
     {
@@ -40,4 +48,23 @@ public class HomeController : Controller
         var rnd = new Random(DateTime.Now.Millisecond);
         return Enumerable.Range(0, 100).Select(x => $"why, hello {id} {rnd.Next(20000)}").ToArray();
     }
+
+[Route("/contacts")]
+    public async Task<IActionResult> GetContacts()
+    {
+    var connstring = configuration["DBInfo:ConnectionString"];
+    var connection =  new NpgsqlConnection(connstring);
+     using (IDbConnection dbConnection = connection)
+            {
+                dbConnection.Open();
+                var r = await dbConnection.QueryAsync<Contact>("select * from Contact;");
+                return new ObjectResult(r);
+            }
+    }
+}
+
+public class Contact
+{
+    public Guid id { get; set; }
+    public DateTime  date { get; set; }
 }
---
+++ src/firstcore/sqlscriptws/create.sql
@@ -0,0 +1,42 @@
+﻿CREATE EXTENSION IF NOT EXISTS "uuid-ossp";
+
+create role coreutil with SUPERUSER LOGIN;
+alter role coreutil with password 'coreutil';
+
+drop table if exists cycle;
+drop table if exists contact;
+
+CREATE TABLE contact
+(
+	id uuid primary key not null,
+	date date not null
+);
+
+CREATE TABLE cycle
+(
+  id uuid not null,
+  data json not null,
+  CONSTRAINT contactfk FOREIGN KEY (id)
+      REFERENCES contact (id) MATCH SIMPLE
+      ON UPDATE CASCADE ON DELETE NO ACTION
+);
+
+insert into contact (id, date) values (uuid_generate_v1(), now());
+insert into contact (id, date) values (uuid_generate_v1(), now());
+insert into contact (id, date) values (uuid_generate_v1(), now());
+insert into contact (id, date) values (uuid_generate_v1(), now());
+insert into contact (id, date) values (uuid_generate_v1(), now());
+insert into contact (id, date) values (uuid_generate_v1(), now());
+insert into contact (id, date) values (uuid_generate_v1(), now());
+insert into contact (id, date) values (uuid_generate_v1(), now());
+insert into contact (id, date) values (uuid_generate_v1(), now());
+insert into contact (id, date) values (uuid_generate_v1(), now());
+
+
+-- insert into cycle (id, data) values (select top 1 id from contact, '{}');
+
+insert into cycle
+	select id, '{}' from contact limit 1;
+
+--select * from contact;
+select * from cycle;
